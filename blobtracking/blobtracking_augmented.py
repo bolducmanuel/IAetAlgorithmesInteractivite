b002802e852aaa27d584862488991cd4a96dd8c9
bolducmanuel <mbolduc@sat.qc.ca>
2024-11-25 16:54:42 -0500

    cours no2

--- blobtracking/blobtracking_augmented.py
+++ blobtracking/blobtracking_augmented.py
@@ -1,25 +1,73 @@
 import cv2
 import math
 
-def is_in_zone(position, zone_centers = [[20,25],[250, 540], [500,300],[90, 400]], zone_rad = [25, 45, 90, 100]):
+# Import needed modules from osc4py3
+from osc4py3.as_eventloop import *
+from osc4py3 import oscbuildparse
 
-    for index, zone in enumerate(zone_centers):
 
-        if math.dist(position, zone) < zone_rad[index]:
+def is_in_zone(keypoints, zone_centers = [[250,250],[500, 250], [500,500],[250, 500]], zone_rad = [50, 50, 50, 50]):
 
-            print("I'm in zone", index, "!")
+    detected_zone = -1
+    
+    for keypoint in keypoints:
+
+        position = keypoint.pt
+
+        for index in range(len(zone_rad)):
+
+            if math.dist(position, zone_centers[index]) < zone_rad[index]:
+
+                detected_zone = index
+                break
+    
+        if detected_zone != -1:
+            break
+
+    # for range purposes
+    detected_zone = (detected_zone + 1.0)/4.0
+
+    return detected_zone 
+
+def proximity_calc(keypoints, distance_prev):
+
+    distance = 0. 
+
+    for keypoint1 in keypoints:
+
+        position1 = keypoint1.pt
+
+        for keypoint2 in keypoints: 
+
+            position2 = keypoint2.pt
+
+            distance += math.dist(position1, position2)
+
+    distance_prev = distance_prev[-1:] + distance_prev[:-1]
+
+    distance_prev[0] = distance
+
+    distance_smooth = math.fsum(distance_prev)/len(distance_prev)
+
+    return distance_smooth, distance_prev
+
+
+
+
+
+
 
 # Create a SimpleBlobDetector parameters object
 params = cv2.SimpleBlobDetector_Params()
 
 # Modify the parameters as needed
 params.minThreshold = 10
-params.maxThreshold = 50
+params.maxThreshold = 100
 params.filterByArea = True
-params.minArea = 10
+params.minArea = 5
 params.filterByColor = True
 params.blobColor = 255
-params.minDistBetweenBlobs = 80
+params.minDistBetweenBlobs = 20
 params.filterByCircularity = False
 params.filterByInertia = False
 params.filterByConvexity = False
@@ -28,71 +76,101 @@ params.filterByConvexity = False
 # Create a SimpleBlobDetector with the parameters
 detector = cv2.SimpleBlobDetector_create(params)
 
+def detection_context():
+    # Open a video capture object
+    cap = cv2.VideoCapture('/home/metalab_legion/Videos/blobtracking_demo/circle_uncompressed.mp4')  # Replace 'your_video_file.avi' with your video file path
 
-# Open a video capture object
-cap = cv2.VideoCapture('/home/metalab_legion/Videos/blobtracking_demo/Bruno_with_light.mp4')  # Replace 'your_video_file.avi' with your video file path
-
-if not cap.isOpened():
-    print("Error: Could not open video file")
-    exit()
-
-
-#initialize keypoints
-keypoints = ([[]])
-
-has_background_img = False
+    if not cap.isOpened():
+        print("Error: Could not open video file")
+        exit()
 
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+    #initialize keypoints
+    keypoints = ([[]])
+
+    has_background_img = False
+
+    frame_count = 0
+
+    # to change the smoothing behavior of the function proximity_calc, add 0s to the list!
+    distance_prev = [0]
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+
+        ## SOUSTRACTION DYNAMIQUE ##
+        # if has_background_img:
+
+        #     temp_background_img = frame
+        #     frame = cv2.subtract(frame, background_img)
+        #     background_img = temp_background_img
+
+        # if not has_background_img:
+
+        #     background_img = frame  
+        #     has_background_img = True 
+
+        ## SOUSTRACTION INITIALE ##
+        if frame_count == 0:
+            background_image = frame
+            frame_count += 1
+
+        frame = cv2.subtract(frame, background_image)
     
-    if has_background_img:
+        keypoints = detector.detect(frame)
 
-        temp_background_img = frame
-        frame = cv2.subtract(frame, background_img)
-        background_img = temp_background_img
+        #detected_zone function call
+        #detected_zone = is_in_zone(keypoints)
+        #print(zone)
 
-    if not has_background_img:
+        #total distance function call
 
-        background_img = frame  
-        has_background_img = True 
+        total_dist, distance_prev = proximity_calc(keypoints, distance_prev)
 
-    
-   
-    keypoints = detector.detect(frame)
-
-    for keypoint in keypoints:
-        is_in_zone(keypoint.pt)
-
-    # Draw the keypoints on the frame
-    frame_with_keypoints = cv2.drawKeypoints(frame, keypoints, outImage=None, color=(0, 0, 255),
-                                             flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    for id, keypoint in enumerate(keypoints):
-        cv2.putText(
-            frame_with_keypoints,
-            str(id),
-            (int(keypoint.pt[0]), int(keypoint.pt[1])),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255,255,255),
-            5,
-            cv2.LINE_AA
-        )
+        osc_process()
+        osc_msg = oscbuildparse.OSCMessage("/zone", None, [total_dist] )
+        osc_send(osc_msg, "localhost")
 
 
-    # Display the frame with keypoints
-    #cv2.imshow('Frame with Keypoints', frame_with_keypoints)
-    cv2.imshow('frame', frame_with_keypoints)
+        
 
-    #cv2.imshow('frame with background removed', mask)
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Draw the keypoints on the frame
+        frame_with_keypoints = cv2.drawKeypoints(frame, keypoints, outImage=None, color=(0, 0, 255),
+                                                flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    cv2.waitKey(33)
-# Release the video capture object and close the OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+        for id, keypoint in enumerate(keypoints):
+            cv2.putText(
+                frame_with_keypoints,
+                str(id),
+                (int(keypoint.pt[0]), int(keypoint.pt[1])),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255,255,255),
+                5,
+                cv2.LINE_AA
+            )
+
+
+        # Display the frame with keypoints
+        #cv2.imshow('Frame with Keypoints', frame_with_keypoints)
+        cv2.imshow('frame', frame_with_keypoints)
+
+        #cv2.imshow('frame with background removed', mask)
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        cv2.waitKey(25)
+    # Release the video capture object and close the OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+    osc_terminate()
+
+if __name__ == '__main__':
+
+    osc_startup()
+    osc_udp_client("127.0.0.1", 9000, "localhost")
+    detection_context()
--- opencv_properties/opencv_guidelines.py
+++ opencv_properties/opencv_guidelines.py
@@ -6,9 +6,12 @@ cap = cv2.VideoCapture(0) #normalement, la webcam
 
 #cap = cv2.VideoCapture(1) # une deuxième caméra USB connectée à l'ordi
 
-cap = cv2.VideoCapture('/home/metalab_legion/Videos/blobtracking_demo/30fps_Bruno_with_light.avi')
+cap = cv2.VideoCapture('/home/metalab_legion/Videos/blobtracking_demo/test_video.mp4')
 
 
+## algorithmes pour la soustraction d'arrière-plans dynamique
+backSub1 = cv2.createBackgroundSubtractorMOG2()
+backSub2 = cv2.createBackgroundSubtractorKNN()
 
 while cap.isOpened():
 
@@ -19,7 +22,16 @@ while cap.isOpened():
 
         continue
 
-    ## METHODE DE SOUSTRACTION D'ARRIERE PLAN ##
+
+    ## RESIZE ##
+
+    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # frame = cv2.resize(frame, (int(0.5*width), int(0.5*height)))
+
+   
+    ## METHODE DE SOUSTRACTION D'ARRIERE PLAN no. 1 ##
     
     # if has_background_img:
 
@@ -32,14 +44,19 @@ while cap.isOpened():
     #     background_img = frame  
     #     has_background_img = True 
 
-    ## METHODE DE SOUSTRACTION D'ARRIERE PLAN ##
+    ## METHODE DE SOUSTRACTION D'ARRIERE PLAN à l'aide d'algorithmes##
+
+    # fgmask1 = backSub1.apply(frame)
+    # fgmask2 = backSub2.apply(frame)
+
+
 
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
-    #cv2.waitKey(33) pour rajouter un délai sur l'exécution de la boucle while
+    #cv2.waitKey(30) #pour rajouter un délai sur l'exécution de la boucle while
 
 cap.release()
 cv2.destroyAllWindows()
